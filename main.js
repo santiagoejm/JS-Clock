208c12b4f724978523ae19107e43fabfd363b147
Santiago Julian <sjulian983@gmail.com>
2022-01-26 15:51:19 +0100

    second version

--- main.js
+++ main.js
@@ -14,6 +14,51 @@ const unifyTime = (num) => {
   return num;
 };
 
+const getDate = () => {
+  let date = new Date();
+
+  let day = date.getDate();
+  if (day < 10) {
+    day = `0${day}`;
+  }
+
+  let month = date.getMonth();
+  month = month + 1;
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  let week = date.getDay();
+  switch (week) {
+    case 0:
+      week = "SUN";
+      break;
+    case 1:
+      week = "MON";
+      break;
+    case 2:
+      week = "TUE";
+      break;
+    case 3:
+      week = "WED";
+      break;
+    case 4:
+      week = "THU";
+      break;
+    case 5:
+      week = "FRI";
+      break;
+    case 6:
+      week = "SAT";
+      break;
+  }
+
+  let fullDate = `${week}-${day}.${month}.${date.getFullYear()}`;
+  fullDateItem.innerText = fullDate;
+};
+
 const time = document.getElementById("time");
+const fullDateItem = document.getElementById("dates");
 
 setInterval(getTime, 1000);
+setInterval(getDate, 1000);
